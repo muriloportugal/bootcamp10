7396581d49f7f005b25c167b21de41893e5c6aef
Murilo Portugal <murilo.portugal@gmail.com>
2020-02-19 22:18:59 -0300

    Desafio2- Usuário Admin

---
+++ desafio2/src/app/models/Users.js
@@ -0,0 +1,30 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init (
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.STRING,
+        password_txt: Sequelize.VIRTUAL,
+      },
+      {
+        sequelize,
+      }
+    );
+    this.addHook('beforeSave', async user => {
+      if (user.password_txt) {
+        user.password = await bcrypt.hash( user.password_txt, 8);
+      }
+    });
+    return this;
+  }
+
+  checkPassword(password_txt){
+    return bcrypt.compare(password_txt, this.password);
+  }
+}
+
+export default User;
